188b908ab5cfd4cd3a883d24cc9b0171f9533430
Herman Ye <hermanye233@icloud.com>
2024-06-18 20:37:10 +0800

    Update for isaac sim config script repeat bug fix

--- scripts/config_isaac_sim_environment.sh
+++ scripts/config_isaac_sim_environment.sh
@@ -2,6 +2,7 @@
 # set -x
 set -e
 # Herman Ye@Auromix
+# 2024-06-18
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # echo script_dir: $script_dir
@@ -10,6 +11,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ISAAC_SIM_VERSION="isaac-sim-4.0.0"
 ISAAC_SIM_PATH="${HOME}/.local/share/ov/pkg/${ISAAC_SIM_VERSION}"
 ISAAC_SIM_PYTHON_EXE="${ISAAC_SIM_PATH}/python.sh"
+ISAAC_SIM_APP_EXE="${ISAAC_SIM_PATH}/isaac-sim.sh"
 BASHRC="${HOME}/.bashrc"
 # echo isaac_sim_path: $ISAAC_SIM_PATH
 # echo isaac_sim_python_exe: $ISAAC_SIM_PYTHON_EXE
@@ -25,27 +27,27 @@ START_MARKER="# Isaac Sim Environment Config [Start]"
 END_MARKER="# Isaac Sim Environment Config [End]"
 
 if grep -qF "$START_MARKER" "$BASHRC"; then
-    echo "Existing Isaac Sim configuration found in ~/.bashrc."
-    read -p "Do you want to overwrite it? (y/n): " choice
-    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-        sed -i "/$START_MARKER/,/$END_MARKER/d" "$BASHRC"
-    else
-        echo "No changes made to ~/.bashrc."
-        exit 0
-    fi
+    echo "Existing Isaac Sim configuration found in ~/.bashrc. Skipping configuration."
+else
+    # Backup bashrc
+    cp ~/.bashrc ~/.bashrc.bak
+    echo "Bashrc backup created at ~/.bashrc.bak"
+    # Add new configurations
+    {
+        echo ""
+        echo $START_MARKER
+        echo "ISAAC_SIM_VERSION=$ISAAC_SIM_VERSION"
+        echo "export ISAAC_SIM_PATH=${ISAAC_SIM_PATH}"
+        echo "export ISAAC_SIM_PYTHON_EXE=${ISAAC_SIM_PYTHON_EXE}"
+        echo "export ISAAC_SIM_APP_EXE=${ISAac_SIM_APP_EXE}"
+        echo "alias omni_python=${ISAAC_SIM_PYTHON_EXE}"
+        echo "alias isaac_sim=${ISAAC_SIM_APP_EXE}"
+        echo $END_MARKER
+        echo ""
+    } >>"$BASHRC"
 fi
 
-# Add new configurations
-{
-    echo ""
-    echo "$START_MARKER"
-    echo "ISAAC_SIM_VERSION=\"$ISAAC_SIM_VERSION\""
-    echo "export ISAAC_SIM_PATH=\"${ISAAC_SIM_PATH}\""
-    echo "export ISAAC_SIM_PYTHON_EXE=\"${ISAAC_SIM_PYTHON_EXE}\""
-    echo "alias omni_python=\"\${ISAAC_SIM_PYTHON_EXE}\""
-    echo "$END_MARKER"
-    echo ""
-} >>"$BASHRC"
-
+echo ""
 echo "Isaac Sim environment configured successfully."
-echo "Please run omin_python <your_python_script_for_isaac_sim> to use Isaac Sim with Python."
+echo "Please run omni_python <your_python_script_for_isaac_sim> to use Isaac Sim with Python."
+echo "Please run isaac_sim to use Isaac Sim GUI."
